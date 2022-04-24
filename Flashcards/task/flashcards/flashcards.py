008b5b95b64dbd591003646df8ec2e10b2d079a6
liuyunan <liuyunan.20@gmail.com>
2022-04-24 00:53:55 -0700

    Your program should read two lines from the console, a term, and a definition, that represent a card.
    After that, the user inputs a line as an answer (a definition of the term on the card). Compare the user's answer with the correct definition and print the result.
    
    The output of the program must contain one of two words:
    
    wrong if the answer doesn't match the definition;
    right if the answer matches the definition.

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,4 +1,7 @@
-print("Card:")
-print("purchase")
-print("Definition:")
-print("buy")
+term = input()
+definition = input()
+answer = input()
+if definition == answer:
+    print("Your answer is right!")
+else:
+    print("Your answer is wrong...")
