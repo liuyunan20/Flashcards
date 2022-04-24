f23074ca344113acba80bbed608fdbc842052eca
liuyunan <liuyunan.20@gmail.com>
2022-04-24 01:05:58 -0700

    Your program should do the following:
    Get the number of flashcards the user would like to create. To do that, print the line Input the number of cards: as a prompt for the user, and then read the number from the next line.
    Create the defined amount of cards in a loop. To create a flashcard, print the line The term for card #n: where n is the index number of the card to be created; then read the user's input (the term) from the following line. Then print the line The definition for card #n: and read the user's definition of the term from the next line. Repeat until all the flashcards are created.
    Test the user on their knowledge of the definitions of all terms in the order they were added. To do that with one flashcard, print the line Print the definition of "term": where "term" is the term of the flashcard to be checked, and then read the user's answer from the following line. Make sure to put the term of the flashcard in quotes. Then print the line Correct! if the user's answer is correct, or the line Wrong. The right answer is "definition". if the answer is incorrect, where "definition" is the correct definition. Repeat for all the flashcards in the set.

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,7 +1,12 @@
-term = input()
-definition = input()
-answer = input()
-if definition == answer:
-    print("Your answer is right!")
-else:
-    print("Your answer is wrong...")
+card_num = input("Input the number of cards:\n")
+term_def = {}
+for i in range(int(card_num)):
+    term = input(f"The term for card #{i + 1}:\n")
+    definition = input(f"The definition for card #{i + 1}:\n")
+    term_def[term] = definition
+for term in term_def:
+    answer = input(f'Print the definition of "{term}"\n')
+    if answer == term_def[term]:
+        print("Correct!")
+    else:
+        print(f'Wrong. The right answer is "{term_def[term]}".')
