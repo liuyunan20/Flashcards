cee20d553cd21da2e4ab498b6ecbc6a176925642
liuyunan <liuyunan.20@gmail.com>
2022-04-24 21:19:35 -0700

    Modify your program and add the following functionalities:
    When the user tries to add a duplicate term, forbid it and output the message The term "term" already exists. Try again: using the term instead of "term". Ask for the term until the user inputs a unique term.
    When the user tries to add a duplicate definition, forbid it and Output the message The definition "definition" already exists. Try again: with the definition instead of "definition". Ask the player to input the definition until the user inputs a unique one.
    When the user enters the wrong definition for the requested term, but this definition is correct for another term, print the appropriate message: Wrong. The right answer is "correct answer", but your definition is correct for "term for user's answer". , where "correct answer" is the actual definition for the requested term, and "term for user's answer" is the appropriate term for the user-entered definition.

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,12 +1,20 @@
 card_num = input("Input the number of cards:\n")
 term_def = {}
+def_term = {}
 for i in range(int(card_num)):
     term = input(f"The term for card #{i + 1}:\n")
+    while term_def.get(term) is not None:
+        term = input(f'The term "{term}" already exists. Try again:\n')
     definition = input(f"The definition for card #{i + 1}:\n")
+    while def_term.get(definition) is not None:
+        definition = input(f'The definition "{definition}" already exists. Try again:\n')
     term_def[term] = definition
+    def_term[definition] = term
 for term in term_def:
     answer = input(f'Print the definition of "{term}"\n')
     if answer == term_def[term]:
         print("Correct!")
+    elif def_term.get(answer) is not None:
+        print(f'Wrong. The right answer is "{term_def[term]}", but your definition is correct for "{def_term[answer]}".')
     else:
         print(f'Wrong. The right answer is "{term_def[term]}".')
